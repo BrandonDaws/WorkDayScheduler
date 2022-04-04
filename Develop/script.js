c0ac31e6a77027e2e023fc60f0fcafc18df887ca
velarde.brandon02@gmail.com <velarde.brandon02@gmail.com>
2022-04-03 23:14:39 -0500

    need to figure out how to set up for loop to check if current time is past the hour blocks time value

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,4 @@
-var CurrentDate = moment().format('dddd');
- console.log(CurrentDate);
+var CurrentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
 document.getElementById("currentDay").innerHTML = CurrentDate;
 
 var timeForDay = ["9 am", "10 am", "11 am", "12 am", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
@@ -9,8 +8,10 @@ var hourBlock;
 var eventBlock;
 var saveButton;
 
+
+
 function displaySchedule() {
-    $.each(timeForDay, function(index ,time){
+    $.each(timeForDay, function(index,time){
       //section for time on left
         timeBlock = $("<section>");
         timeBlock.addClass("row");
@@ -21,17 +22,29 @@ function displaySchedule() {
         hourBlock.css("padding-top","31.5px");
         hourBlock.text(time);
 
-        eventBlock = $("<div>");
-        eventBlock.addClass("description col-2 col-lg-1")
+        eventBlock = $("<textarea>");
+        eventBlock.addClass("description col-8");
+        eventBlock.css("padding-right", "31.5px");
+        eventBlock.text('');
+        
+        
+        
+
         
         
 
+        saveButton = $("<button>")
+        saveButton.addClass("saveBtn col-2");
+        
+        
+        
 
 
 
 
 
-        timeBlock.append(hourBlock);
+
+        timeBlock.append(hourBlock,eventBlock,saveButton);
 
     $(".container").append(timeBlock);
 
