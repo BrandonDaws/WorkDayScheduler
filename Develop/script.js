e6db889b9aac1b540c8caccd753020542e6d460e
BrandonDaws <velarde.brandon02@gmail.com>
2022-06-23 09:52:46 -0500

    got time function and local storage worked out. ready to turn in mvp.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 var CurrentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
-document.getElementById("currentDay").innerHTML = CurrentDate;
+document.getElementById("currentDay").innerHTML = "Todays Date: " + CurrentDate;
 
 var timeForDay = ["9 am", "10 am", "11 am", "12 am", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
 var timeValue = [09, 10, 11, 12, 13, 14, 15, 16, 17];
@@ -8,6 +8,8 @@ var hourBlock;
 var eventBlock;
 var saveButton;
 var currentTime = moment().format("HH")
+
+
 
 
 function displaySchedule() {
@@ -31,10 +33,14 @@ function displaySchedule() {
 
         
         saveButton = $("<button>")
-        saveButton.addClass("saveBtn fa fa-save col-2");
-        saveButton.on("click", function(){
-            localStorage.setItem("task", eve)
-        });
+        saveButton.addClass("saveBtn fa fa-save col-2")
+        saveButton.attr("type", "submit")
+        saveButton.attr("id", timeValue[index]);
+     //   saveButton.on("click", function(){
+          //  localStorage.setItem("task",  )
+
+            
+      //  });
         
         timeBlock.append(hourBlock, eventBlock, saveButton);
         $(".container").append(timeBlock,);
@@ -49,32 +55,39 @@ function checkTime() {
   var now = moment()
     //var currentTime = moment().format('HH');
   
-    $.each(timeValue, function (index, time) {
+    $.each(timeValue, function (index) {
         eventBlock = $("#" + timeValue[index]);
 
-        if (eventBlock[0].id < now.hour()) {
+        if ((parseInt(eventBlock[0].id) < now.hour())) {
             eventBlock.addClass("past");
 
-        } else if (eventBlock[0].id === now.hour()) {
+        } else if ((parseInt(eventBlock[0].id) === now.hour())) {
             eventBlock.addClass("present")
  
         } else {
             eventBlock.addClass("future")
         };
-
+        
+        eventBlock.text(localStorage.getItem("eventEntry" + timeValue[index]))
     });
+  
 }
 
 function saveEvents(event){
     event.preventDefault()
     var itemId = event.currentTarget.id
     var itemValue =  event.currentTarget.previousSibling.value
-    localStorage.setItem("eventEntry" + event.currentTarget.id, event.currentTarget.previousSibling.value);
-    console.log(itemId);
-    console.log(itemValue);
+    localStorage.setItem("eventEntry" + itemId, itemValue);
+   // console.log(itemId);
+    //console.log(itemValue);
+
 }
+
+
+//save task function
 
 
 
 displaySchedule();
 checkTime();
+$(".saveBtn").click(saveEvents)
